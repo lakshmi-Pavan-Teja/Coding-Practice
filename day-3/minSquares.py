d5da39d59b7c5acd2d9fa408214933217e148b13
lakshmi-Pavan-Teja <61023747+lakshmi-Pavan-Teja@users.noreply.github.com>
2020-07-26 21:06:01 +0530

    Add files via upload

---
+++ day-3/minSquares.py
@@ -0,0 +1,14 @@
+t = int(input())
+for i in range(t):
+    a,b = list(map(int,input().split()))
+    count = 0
+    while a!=0 or b!=0:
+        if a>b:
+            a -=b
+        elif a<b:
+            b -=a
+        else:
+            a = 0
+            b = 0
+        count += 1
+    print(count)
