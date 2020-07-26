e1fb52f52478e49ee52d1340d776539844759ad4
lakshmi-Pavan-Teja <61023747+lakshmi-Pavan-Teja@users.noreply.github.com>
2020-07-26 21:03:55 +0530

    Add files via upload

---
+++ day-3/internetUsuage.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int tc,n,k,t,d,i,charge;
+	scanf("%d",&tc);
+	while(tc--)
+	{
+		charge=0;
+		scanf("%d%d",&n,&k);
+		for(i=0;i<n;i++)
+		{
+			scanf("%d%d",&t,&d);
+			if(k>t)
+			{
+				k=k-t;
+			}
+			else
+			{
+				charge=charge+(t-k)*d;
+				k=0;
+			}
+		}
+		printf("%d\n",charge);
+	}
+	return 0;
+}
---
+++ day-3/minMilkBottles.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j=0,k,a[10],b[10]={0};
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	while(j<n)
+	{
+		while(a[j]!=0)
+		{
+			if(a[j]>=10)
+			{
+				a[j]=a[j]-10;
+				b[j]++;
+			}
+			else if(a[j]>=7)
+			{
+				a[j]=a[j]-7;
+				b[j]++;
+			}
+			else if(a[j]>=5)
+			{
+				a[j]=a[j]-5;
+				b[j]++;
+			}
+			else if(a[j]>=1)
+			{
+				a[j]=a[j]-1;
+				b[j]++;
+			}
+		}
+		j++;
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d  ",b[i]);
+	}
+}
